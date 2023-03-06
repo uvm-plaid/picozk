c7e40e526ba9d4cea9d42bd08fba6c92781ae028
Joe Near <jnear@uvm.edu>
2023-03-06 14:53:51 -0500

    encapsulate compiler in an object

--- picowizpl.py
+++ picowizpl.py
@@ -4,19 +4,11 @@ from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 
-relation_file = None
-witness_file = None
+# Current compiler
+cc = None
 
-def emit(s=''):
-    global relation_file
-    relation_file.write(s)
-    relation_file.write('\n')
-
-def add_to_witness(x):
-    r = next_wire()
-    emit(f'  {r} <- @private;')
-    witness_file.write(f'  < {x} >;\n')
-    return Wire(r, x)
+def SecretInt(x):
+    return cc.add_to_witness(x)
 
 def val_of(x):
     if isinstance(x, Wire):
@@ -27,59 +19,69 @@ def val_of(x):
     else:
         return x
 
-def wire_of(e):
-    if isinstance(e, Wire):
-        return e.wire
-    elif isinstance(e, int):
-        r = next_wire()
-        emit(f'  {r} <- <{e}>;')
-        return r
-
-current_wire = 0
-def next_wire():
-    global current_wire
-    r = current_wire
-    current_wire += 1
-    return '$' + str(r)
-
-def SecretInt(x):
-    return add_to_witness(x)
-
-
 @dataclass
 class Wire:
     wire: str
     val: any
 
     def __add__(self, other):
-        r = next_wire()
-        emit(f'  {r} <- @add({self.wire}, {wire_of(other)});')
+        r = cc.next_wire()
+        cc.emit(f'  {r} <- @add({self.wire}, {cc.wire_of(other)});')
         return Wire(r, self.val + val_of(other))
 
     def __radd__(self, other):
-        r = next_wire()
-        emit(f'  {r} <- @add({self.wire}, {wire_of(other)});')
+        r = cc.next_wire()
+        cc.emit(f'  {r} <- @add({self.wire}, {cc.wire_of(other)});')
         return Wire(r, self.val + val_of(other))
 
     def __mul__(self, other):
-        r = next_wire()
-        emit(f'  {r} <- @mul({self.wire}, {wire_of(other)});')
+        r = cc.next_wire()
+        cc.emit(f'  {r} <- @mul({self.wire}, {cc.wire_of(other)});')
         return Wire(r, self.val * val_of(other))
 
     def __rmul__(self, other):
-        r = next_wire()
-        emit(f'  {r} <- @mul({self.wire}, {wire_of(other)});')
+        r = cc.next_wire()
+        cc.emit(f'  {r} <- @mul({self.wire}, {cc.wire_of(other)});')
         return Wire(r, self.val * val_of(other))
 
 
-def open_picowizpl(filename):
-    global witness_file
-    global relation_file
-    witness_file = open(filename + '.type0.wit', 'w')
-    relation_file = open(filename + '.rel', 'w')
+class PicoWizPLCompiler(object):
+    def __init__(self, file_prefix):
+        self.file_prefix = file_prefix
+        self.current_wire = 0
 
-def close_picowizpl():
-    global witness_file
-    global relation_file
-    witness_file.close()
-    relation_file.close()
+    def emit(self, s=''):
+        self.relation_file.write(s)
+        self.relation_file.write('\n')
+
+    def add_to_witness(self, x):
+        r = self.next_wire()
+        self.emit(f'  {r} <- @private;')
+        self.witness_file.write(f'  < {x} >;\n')
+        return Wire(r, x)
+
+    def wire_of(self, e):
+        if isinstance(e, Wire):
+            return e.wire
+        elif isinstance(e, int):
+            r = self.next_wire()
+            self.emit(f'  {r} <- <{e}>;')
+            return r
+
+    def next_wire(self):
+        r = self.current_wire
+        self.current_wire += 1
+        return '$' + str(r)
+
+    def __enter__(self):
+        global cc
+        cc = self
+        self.relation_file = open(self.file_prefix + '.type0.wit', 'w')
+        self.witness_file = open(self.file_prefix + '.rel', 'w')
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        global cc
+        self.relation_file.close()
+        self.witness_file.close()
+        cc = None
+
