0ef9d3e783d7fafa3f346d335d94fb4bb1d2677e
Joe Near <jnear@uvm.edu>
2023-03-10 10:42:32 -0500

    working towards compiling functions

--- binary.py
+++ binary.py
@@ -5,6 +5,10 @@ import galois
 gf_2 = galois.GF(2)
 BITWIDTH = 32
 
+def abs_add(wires, a, b):
+    vals = encode_int(a.to_int() + b.to_int())
+    return BinaryInt([Wire(w, v) for w, v in zip(wires, vals)])
+
 @dataclass
 class BinaryInt:
     wires: List[Wire]
@@ -13,6 +17,7 @@ class BinaryInt:
         val_list = [int(val_of(x)) for x in self.wires]
         return int("".join(str(x) for x in reversed(val_list)), 2)
 
+    @picowizpl_function(in_wires=BITWIDTH, out_wires=BITWIDTH, abstraction=abs_add)
     def __add__(self, other):
         out_wires = []
         carry = 0
@@ -39,9 +44,10 @@ def check_equal(c, i):
         assert0(w + b)
 
 with PicoWizPLCompiler('miniwizpl_test', field=2):
+    print('enter')
     v = binary_int(10)
     tot = v
-    for i in range(2000):
+    for i in range(20):
         tot = tot + v
     #print(tot)
     print(tot.to_int())
