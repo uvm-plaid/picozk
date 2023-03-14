bd9f7fabbf64ba146339273d30e8733bfaa2f79c
Joe Near <jnear@uvm.edu>
2023-03-14 09:36:29 -0400

    remove dask example

--- examples/example_dask_dataframe.py
+++
@@ -1,9 +0,0 @@
-import dask
-from picowizpl import *
-
-with PicoWizPLCompiler('miniwizpl_test'):
-    df = dask.datasets.timeseries()
-    s = df['id'].apply(SecretInt)
-    #print(len(s))
-    print(s.sum().compute())
-
