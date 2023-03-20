97a54b434309f25c398236d53f832e6cbc8b6c24
Joe Near <jnear@uvm.edu>
2023-03-20 06:59:03 -0400

    simplify mlp example

--- examples/example_pytorch.py
+++ examples/example_pytorch.py
@@ -9,14 +9,12 @@ import torch.optim as optim
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(784, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 10)
+        self.fc1 = nn.Linear(784, 32)
+        self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.fc2(x)
-        x = self.fc3(x)
         return x
 
 model = Net()
