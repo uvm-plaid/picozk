7e0c97d6e9b868ba1fe306191f8b5bbf2cdfa7dc
Joe Near <jnear@uvm.edu>
2023-03-20 10:07:55 -0400

    add sparse matrix-vector product example

--- examples/bench_sparsemat.py
+++ examples/bench_sparsemat.py
@@ -1,28 +1,35 @@
 from picowizpl import *
 import numpy as np
 from scipy.sparse import csr_matrix
+from dataclasses import dataclass
+
+from typing import Dict, Tuple
 
 N = 20
 K = 5
 
+def sparse_mult(sparse_mat: Dict[Tuple[ArithmeticWire, ArithmeticWire], ArithmeticWire],
+                vec: SecretIndexList) -> SecretIndexList:
+    output = SecretIndexList([0 for _ in range(len(vec))])
+
+    for (i, j), v in sparse_mat.items():
+        output[i] = output[i] + secret_vec[j] * v
+
+    return output
+
 with PicoWizPLCompiler('miniwizpl_test', options=['ram']):
     arr = np.zeros((N, N))
-    sparse_arr = {}
+    sparse_mat = {}
     all_is = np.random.choice(range(0, N), size=K)
     all_js = np.random.choice(range(0, N), size=K)
     for i, j in zip(all_is, all_js):
         v = np.random.randint(0, 100)
         arr[i, j] = v
-        sparse_arr[(SecretInt(i), SecretInt(j))] = SecretInt(v)
-
-    print(sparse_arr)
+        sparse_mat[(SecretInt(i), SecretInt(j))] = SecretInt(v)
 
     vec = np.random.randint(0, 5, size=N)
     secret_vec = SecretIndexList(list(vec))
-    output = SecretIndexList([0 for _ in range(N)])
 
-    for (i, j), v in sparse_arr.items():
-        output[i] = output[i] + secret_vec[j] * v
-
-    # print(output)
-    # print(arr @ vec == output)
+    output = sparse_mult(sparse_mat, secret_vec)
+    for i in range(len(output)):
+        reveal(output[i])
