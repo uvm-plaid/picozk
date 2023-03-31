5904ba26c1b6ef985f1e3594dff1e2c53e7d2e1e
Joe Near <jnear@uvm.edu>
2023-03-31 14:33:25 -0400

    add more oblif tests

--- examples/example_oblif.py
+++ examples/example_oblif.py
@@ -8,6 +8,18 @@ def test(x):
     else:
         return x + 5
 
+@oblif
+def test2(x):
+    if x == 1:
+        z = 3
+    elif x == 2:
+        z = 4
+    elif x == 5:
+        z = 6
+    else:
+        z = 20
+    return z + 32
+
 with PicoZKCompiler('picozk_test'):
     x = SecretInt(5)
     r1 = test(x)
@@ -18,3 +30,7 @@ with PicoZKCompiler('picozk_test'):
     r2 = test(y)
     reveal(r2)
     print(r2)
+
+    r3 = test2(x)
+    reveal(r3)
+    print(r3)
