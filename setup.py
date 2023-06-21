355bb7a0857f75845bf023bd7860399eeaa73849
Mark Blunk <mark@stealthsoftwareinc.com>
2023-06-21 11:26:30 -0700

    adding python dependencies to setup file

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(name='picozk',
       author='Joe Near',
       author_email='jnear@uvm.edu',
       license='GPLv3',
-      install_requires=[],
+      install_requires=[
+          "ecdsa==0.18.0",
+          "numpy==1.21.6",
+          "pandas==1.3.5"],
       packages=['picozk', 'picozk/poseidon_hash'],
       zip_safe=False)
