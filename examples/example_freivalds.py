09b7323239cbc7d7d31ce3a8e5e1562655f02afa
Joe Near <jnear@uvm.edu>
2023-09-03 09:48:03 -0400

    fix datatype issue in Freivalds' algorithm

--- examples/example_freivalds.py
+++ examples/example_freivalds.py
@@ -4,21 +4,25 @@ import numpy as np
 # Freivalds' algorithm for probabilistic verification of matrix multiplication
 # See Section 3.3, https://eprint.iacr.org/2021/730.pdf
 
-n = 40
-m = 50
-l = 35
+n = 35
+m = 40
+l = 50
 
 p = 2**61-1
 
-a = np.random.randint(-2**25, 2**25, (n, m))
-b = np.random.randint(-2**25, 2**25, (m, l))
+def int_matrix(m):
+    intv = np.vectorize(int)
+    return np.array(intv(m).tolist(), dtype=object)
+
+a = int_matrix(np.random.randint(0, p, (n, m)))
+b = int_matrix(np.random.randint(0, p, (m, l)))
 c = a @ b
 
 zk_matrix = np.vectorize(SecretInt, otypes=[Wire])
 
 with PicoZKCompiler('picozk_test'):
-    zk_a = zk_matrix(a%p)
-    zk_b = zk_matrix(b%p)
+    zk_a = zk_matrix(a)
+    zk_b = zk_matrix(b)
 
     # Requires 70,000 multiplication gates
     # zk_c = zk_a @ zk_b
@@ -28,7 +32,7 @@ with PicoZKCompiler('picozk_test'):
     u = np.random.randint(0, p, n)
     v = np.random.randint(0, p, l)
 
-    mat1 = u @ zk_a @ zk_b @ v.T
-    mat2 = u @ new_zk_c @ v.T
+    r1 = u @ zk_a @ zk_b @ v.T
+    r2 = u @ new_zk_c @ v.T
 
-    assert0(mat1-mat2)
+    assert0(r1-r2)
